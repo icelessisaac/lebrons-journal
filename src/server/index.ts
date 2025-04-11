520feee8310d0385a3c4b0756f48b4029d1592b9
Neo <99268950+n30w@users.noreply.github.com>
2025-04-12 02:45:57 +0800

    refactor: up env

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,20 @@
+import { IS_DEV } from "@/lib/environment";
 import { createThirdwebClient } from "thirdweb";
 
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
-const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 
-if (!clientId) {
-  throw new Error("No client ID provided");
+let clientId;
+
+clientId = "lmao123456";
+
+if (!IS_DEV) {
+  if (!clientId) {
+    throw new Error("No client ID provided");
+  }
+  clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
 }
 
 export const client = createThirdwebClient({
-  clientId: clientId,
+  clientId: clientId!,
 });
